89371c83e9aa1f5810e990b65214b0ec9009870f
Zapolyarny <zapolyarnynorth@gmail.com>
2026-02-22 13:42:33 +0300

    build(plugin) add api classes to jar task

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,4 +1,14 @@
+import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.jvm.tasks.Jar
+
 dependencies {
     api(project(":api"))
     annotationProcessor(rootProject.libs.velocity.api)
+}
+
+tasks.named<Jar>("jar") {
+    dependsOn(":api:classes")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(project(":api").extensions.getByType(SourceSetContainer::class.java).getByName("main").output)
 }
