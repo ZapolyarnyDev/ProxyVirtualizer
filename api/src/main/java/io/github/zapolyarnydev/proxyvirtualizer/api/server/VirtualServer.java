8910d2ef052dd586f4e510b022bf5eb371d5b0d0
Zapolyarny <zapolyarnynorth@gmail.com>
2026-02-22 13:25:26 +0300

    feat(api): add version packet version control

--- api/src/main/java/io/github/zapolyarnydev/proxyvirtualizer/api/server/VirtualServer.java
+++ api/src/main/java/io/github/zapolyarnydev/proxyvirtualizer/api/server/VirtualServer.java
@@ -1,5 +1,28 @@
 package io.github.zapolyarnydev.proxyvirtualizer.api.server;
 
-public interface VirtualServer {
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
+public interface VirtualServer {
+    String getName();
+
+    Set<Integer> getSupportedProtocolVersions();
+
+    void allowProtocolVersion(int protocolVersion);
+
+    void disallowProtocolVersion(int protocolVersion);
+
+    boolean isProtocolVersionSupported(int protocolVersion);
+
+    PacketVersionRule registerPacketVersion(String packetKey, int protocolVersion, int packetVersion);
+
+    Optional<PacketVersionRule> getPacketVersion(String packetKey, int protocolVersion);
+
+    boolean removePacketVersion(String packetKey, int protocolVersion);
+
+    Map<String, Set<PacketVersionRule>> getPacketVersionMatrix();
+
+    record PacketVersionRule(String packetKey, int protocolVersion, int packetVersion) {
+    }
 }
