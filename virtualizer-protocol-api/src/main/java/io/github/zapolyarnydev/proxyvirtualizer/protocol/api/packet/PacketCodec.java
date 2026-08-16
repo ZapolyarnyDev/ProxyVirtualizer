e900d11b268d6944e42d8984a35a6bb598bca548
Zapolyarny <zapolyarnynorth@gmail.com>
2026-08-16 22:39:50 +0300

    Gave up

--- virtualizer-protocol-api/src/main/java/io/github/zapolyarnydev/proxyvirtualizer/protocol/api/packet/PacketCodec.java
+++ virtualizer-protocol-api/src/main/java/io/github/zapolyarnydev/proxyvirtualizer/protocol/api/packet/PacketCodec.java
@@ -11,6 +11,7 @@ public interface PacketCodec<P extends ProtocolPacket> {
   @NotNull
   P decode(@NotNull ByteBuffer input);
 
+  // And this is where my attempt to document the code ends. I'll come back to it someday
   /** Returns a buffer whose remaining bytes are the complete packet payload. */
   @NotNull
   ByteBuffer encode(@NotNull P packet);
